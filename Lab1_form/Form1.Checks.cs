289050df923ea68fbde1425308f43a9b65917141
unknown <petrarce@gmail.com>
2015-09-22 16:51:20 +0300

    start my project documentation

---
+++ Lab1_form/Form1.Checks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_form_1_
+{
+    partial class Form1
+    {
+        private bool CheckTextBoxes(String[] str){
+            try
+            {
+                for (int i = 0; i > -1; i++)
+                {
+                    if (str[i] == "0") { };//костыль для выхода из цыкла
+                    try
+                    {
+                        for (int j = i + 1; j > i; j++)
+                        {
+                            if (str[i].Equals(str[j]))
+                                return false;
+                        }
+                    }
+                    catch(IndexOutOfRangeException) { }
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return true;
+            }
+            return true;
+        }
+        private bool CheckgetAllInfo() {
+            foreach (newTextBox e in TextBoxes) {
+                if (!e.isCorrectlyChecked)
+                    return false;
+            }
+            return true;
+        }
+        private delegate bool DelegCheckTextBoxes(String[] str);
+
+    }
+}
---
+++ Lab1_form/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApplication1;
+using System.Threading;
+
+namespace Lab1_form_1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent_user();
+        }
+        private void TextEntered(object sender, EventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                DelegCheckTextBoxes currentThread = new DelegCheckTextBoxes(CheckTextBoxes);
+                IAsyncResult reult=currentThread.BeginInvoke((sender as TextBox).Text.Split(' '), null, null);
+                if (!currentThread.EndInvoke(reult)) {
+                    errorProvider1.SetError((sender as newTextBox),"this string has repeateing value");
+                    (sender as newTextBox).isCorrectlyChecked = false;
+                    return;
+                }
+                errorProvider1.Clear();
+                (sender as newTextBox).isCorrectlyChecked = true;
+            }
+        }
+        private void Del_Click(object sender, EventArgs e){
+
+           this.Controls.Remove(TextBoxes[TextBoxes.Count - 1]);
+           this.TextBoxes.RemoveAt(TextBoxes.Count - 1);
+           this.addStr.Location = new System.Drawing.Point(
+                                    this.addStr.Location.X,
+                                    this.addStr.Location.Y - 26);
+           if (TextBoxes.Count == 1)
+            {
+               DelButton.Visible = false;
+               return;
+           }
+           this.DelButton.Location = new System.Drawing.Point(
+                this.DelButton.Location.X,
+                this.DelButton.Location.Y - 26);
+
+
+           
+        }
+
+        private void addStr_Click(object sender, EventArgs e)
+            
+        {
+            if (this.addStr.Location.Y + 100 < this.Size.Height)
+            {
+                NewTextbox();
+                this.addStr.Location = new System.Drawing.Point(
+                                        this.addStr.Location.X,
+                                        this.addStr.Location.Y + 26);
+                if (DelButton.Visible == true) this.DelButton.Location = new System.Drawing.Point(
+                                          this.DelButton.Location.X,
+                                          this.DelButton.Location.Y + 26);
+                else DelButton.Visible = true;
+            }
+
+
+            
+        }
+
+        private void getAllInfo(){
+            List<String[]> tmpVar=new List<String[]>();
+            foreach(TextBox Element in TextBoxes){
+                tmpVar.Add(Element.Text.Split(' '));
+            }
+            mainLabSolver=new lab2solver(){OperList=tmpVar};
+            mainLabSolver.initLabSolver();
+            MatrixTextBox.Text = "K= " + Convert.ToString(mainLabSolver.KElem)+"\n";
+            foreach(Int16[] str in mainLabSolver.Matrix) {
+                foreach (Int16 element in str) {
+                    MatrixTextBox.Text += Convert.ToString(element)+" ";
+                }
+                MatrixTextBox.Text += "\n";
+            }
+            Int16 i=1;
+            foreach (List<short> group in mainLabSolver.Groups) {
+                GroupsTextBox.Text += "Group " + i.ToString()+":";
+                foreach (Int16 element in group) {
+                    GroupsTextBox.Text += element.ToString() + " ";
+                }
+                GroupsTextBox.Text += "\n";
+                i++;
+            }
+            
+
+        }
+
+        private void SolveBut_Click(object sender, EventArgs e)
+        {
+            if (!CheckgetAllInfo()) {
+                errorProvider1.SetError((sender as Button), "check your textboxes for correcr input");
+                return;
+            }
+
+            CreateGroupTextBox();
+            CreateMatrixTextBox();
+            getAllInfo();
+        }
+        private void SolveBut_Leave(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+
+        }
+    }
+}
