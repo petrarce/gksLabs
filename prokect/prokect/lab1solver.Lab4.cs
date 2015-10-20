2612e150d355eaab84281fb1215072d453e0bb88
Ivan-PC <petrarce@gmail.com>
2015-10-20 23:21:11 +0300

    adds function of finding circuits in groups (finds connection matrix, etc...)

--- prokect/prokect/lab1solver.Lab4.cs
+++ prokect/prokect/lab1solver.Lab4.cs
@@ -8,57 +8,181 @@ namespace ConsoleApplication1
 {
     partial class lab1Solver
     {
-        //private List<Matrix> groupsModuls;
-
-        /*public List<Matrix> GroupsModuls
+        public void StartLab4Task()
         {
-            get { return groupsModuls; }
-            set { groupsModuls = value; }
-        }*/
+            InitializeNewElements();
+            for(Int16 i = 0; i < Groups.Count; i++)
+                GetNewModuls(i);
+        }
+        internal void InitializeNewElements() 
+        {
+            for (Int16 GroupNumber = 0; GroupNumber < Groups.Count; GroupNumber++) {
+                InitializeNewElement(GroupNumber);
+            }    
+        }
+            private void InitializeNewElement(Int16 GroupNumber)
+                {
+                    Groups[GroupNumber].Moduls = new List<Modul>();
+                    groups[GroupNumber].ConnectionMatrix = new Matrix();
+                    InitializeFirstModuls(GroupNumber);
+                    GetComunicationMatrix(GroupNumber);
 
-        public void GetNewModuls() {
-            for (Int16 GroupNumber = 0; GroupNumber < Groups.Count; GroupNumber++)
+                }
+                private void InitializeFirstModuls(Int16 GroupNumber) 
             {
-                GetNewModuls(GroupNumber);
+                    foreach (String modulName in Groups[GroupNumber].Operations) {
+                        Groups[GroupNumber].Moduls.Add(new Modul(modulName, new []{modulName}));
+                    }
             }
-        }
-        private void GetNewModuls(Int16 GrupNumber) {
-            GetComunicationMatrix(GrupNumber);
-            GetModuls(GrupNumber);
-        }
-            private void GetComunicationMatrix(Int16 GrupNumber)
-            {
-                CreateMatrix(GrupNumber);
-                InitializeMatrix(GrupNumber);
-            }
-                private void CreateMatrix(Int16 GrupNumber)
+                private void GetComunicationMatrix(Int16 GroupNumber)
                 {
-                    foreach (String operationRow in Groups[GrupNumber].Operations)
+                    CreateMatrix(GroupNumber);
+                    InitializeMatrix(GroupNumber);
+                }
+                    private void CreateMatrix(Int16 GroupNumber)
                     {
-                        Groups[GrupNumber].ConnectionMatrix.Add(operationRow, new Dictionary<String, Boolean>());
-                        foreach (String operationColumn in Groups[GrupNumber].Operations)
+                        foreach (Modul modulNameRow in Groups[GroupNumber].Moduls)
                         {
-                            Groups[GrupNumber].ConnectionMatrix[operationRow].Add(operationColumn, false);
-                        }
-                    }
-                }
-                private void InitializeMatrix(Int16 GrupNumber)
-                {
-                    foreach (Int16 Element in Groups[GrupNumber].Elements)
-                    {
-                        InitializeMatrixForElement(Element,GrupNumber);
-                    }
-                }
-                private void InitializeMatrixForElement(Int16 Currentlement, Int16 GrupNumber)
-                {
-                        String tempElementOperation = null;
-                        foreach (String operation in operList[Currentlement]) {
-                            if (tempElementOperation != null) {
-                                Groups[GrupNumber].ConnectionMatrix[tempElementOperation][operation] = true;
+                            Groups[GroupNumber].ConnectionMatrix.Add(modulNameRow.ModulName, new Dictionary<String, Boolean>());
+                            foreach (Modul modulNameColumn in Groups[GroupNumber].Moduls)
+                            {
+                                Groups[GroupNumber].ConnectionMatrix[modulNameRow.ModulName].Add(modulNameColumn.ModulName, false);
                             }
-                            tempElementOperation = operation;
                         }
                     }
-            private void GetModuls(Int16 GroupNumber) { }
-    }
+                    private void InitializeMatrix(Int16 GroupNumber)
+                    {
+                        foreach (Int16 Element in Groups[GroupNumber].Elements)
+                        {
+                            InitializeMatrixUsingElements(Element, GroupNumber);
+                        }
+                    }
+                    private void InitializeMatrixUsingElements(Int16 Currentlement, Int16 GroupNumber)
+                    {
+                            String tempElementOperation = null;
+                            foreach (String operation in operList[Currentlement]) {
+                                if (tempElementOperation != null) {
+                                    Groups[GroupNumber].ConnectionMatrix[tempElementOperation][operation] = true;
+                                }
+                                tempElementOperation = operation;
+                            }
+                    }
+        
+        
+        
+        private void GetNewModuls(Int16 GroupNumber)
+        {
+            while(GetNewModul(GroupNumber)) { }
+        }
+        private bool GetNewModul(Int16 GroupNumber)
+            {
+               // if(FindWeight(GroupNumber))
+                //    return true;
+                if(CheckChain(GroupNumber))
+                    return true;
+                //CheckInterrelation( GroupNumber);
+                //CheckInOutOnly( GroupNumber);
+                return false;
+            }
+            //Checking for chain existing
+            private bool CheckChain(Int16 GroupNumber)
+            {
+                String Current;
+                List<String> PotentialModul=new List<String>();
+                List<String> tempChecked = new List<String>();//Names of checked moduls
+                foreach(Modul modul in Groups[GroupNumber].Moduls) {
+                    if(ModulInChecked(tempChecked, modul))
+                        continue;
+                    PotentialModul.Clear();
+                    tempChecked.Add(modul.ModulName);
+                    Current = modul.ModulName;
+                    if(!RowAndColWeight(1,1,GroupNumber,Current)) {
+                        continue;
+                    }
+                    do{
+                        PotentialModul.Add(Current);
+                        Current = FindNext(Current, GroupNumber);
+                        tempChecked.Add(modul.ModulName);
+                    } while(RowAndColWeight(1,1,GroupNumber,Current));
+                    if(!RowAndColWeight(1, 0, GroupNumber, Current))//!!!
+                    {
+                        continue;
+                    }
+                    if(Groups[GroupNumber].ConnectionMatrix[PotentialModul[0]][Current] == true ||
+                        Groups[GroupNumber].ConnectionMatrix[Current][PotentialModul[0]] == true)
+                    {
+                        CreateModul(GroupNumber,PotentialModul);
+                        return true;
+                    }
+
+                }
+                return false;
+            }
+                private String FindNext(String Current,Int16 GroupNumber) 
+                {
+                    foreach(var Element in Groups[GroupNumber].ConnectionMatrix[Current]) {
+                        if(Element.Value == true)
+                            return Element.Key;
+                    }
+                    return "";
+                }
+                private void CreateModul(Int16 GroupNumber,List<String> PotentialModul)
+                {
+                    /*Modul tempFirstModul,tempModul;
+                    foreach(var modul in Groups[GroupNumber].Moduls )
+                        if(modul.ModulName==PotentialModul[0]){
+                            tempFirstModul = modul;
+                            break;
+                        }*/
+                    foreach(var Element in PotentialModul) { 
+                        if(Element==PotentialModul[0])
+                            continue;
+                        AddRowsAndColumns(PotentialModul[0], Element, GroupNumber);
+                        //AddColumns(PotentialModul[0], Element);
+                        DeleteModuls(PotentialModul, GroupNumber);
+                    }
+                }
+                    private void AddRowsAndColumns(String FirstModul, String DeletingModul,Int16 GroupNumber)
+                    {
+                        foreach(var element in Groups[GroupNumber].Moduls){
+                            if(Groups[GroupNumber].ConnectionMatrix[DeletingModul][element.ModulName]==true)
+                                Groups[GroupNumber].ConnectionMatrix[FirstModul][element.ModulName] = true;
+                            if(Groups[GroupNumber].ConnectionMatrix[element.ModulName][DeletingModul]==true)
+                                Groups[GroupNumber].ConnectionMatrix[element.ModulName][FirstModul] = true;
+                        }
+                        Groups[GroupNumber].ConnectionMatrix[FirstModul][FirstModul] = false;
+                    }
+                    private void DeleteModuls(List<String> PotentialModul,Int16 GroupNumber){
+                        foreach(var element in PotentialModul)
+                        {
+                            foreach(var row in Groups[GroupNumber].Moduls)
+                            {
+                                Groups[GroupNumber].ConnectionMatrix[row.ModulName].Remove(element);
+                            }
+                            Groups[GroupNumber].ConnectionMatrix.Remove(element);
+                            foreach(var modul in Groups[GroupNumber].Moduls)
+                                if(modul.ModulName==element)
+                                    Groups[GroupNumber].Moduls.Remove(modul);
+                        }
+                    }
+            //checks if modul are in checked
+            private bool ModulInChecked(List<String> tempChecked,Modul modul)
+            {
+                foreach(String CheckItemn in tempChecked) {
+                    if(modul.ModulName == CheckItemn)
+                        return true;
+
+                }
+                return false;
+            }
+            private void FindWeight(Int16 GroupNumber)
+            {
+                Groups[GroupNumber].ConnectionMatrix.FindColWeight();
+                Groups[GroupNumber].ConnectionMatrix.FindRowWeight();
+            }
+            private bool RowAndColWeight(Int16 value1, Int16 value2,Int16 GroupNumber,String Current) 
+            {
+                return Groups[GroupNumber].ConnectionMatrix.RowWeight[Current] == value1 && Groups[GroupNumber].ConnectionMatrix.ColumnWeight[Current] == value2; 
+            }
+    }   
 }
--- prokect/prokect/lab1solver.Types.cs
+++ prokect/prokect/lab1solver.Types.cs
@@ -34,8 +34,24 @@ namespace ConsoleApplication1
         }
 
         public sealed class Modul : OperationsClass {
-            Modul() {
+            private String modulName;
+
+            public String ModulName 
+            {
+                get { return modulName; }
+                set { modulName = value; }
+            }
+
+            public Modul() 
+            {
                 this.operations = new List<string>();
+            }
+            public Modul(String Modulname,String[] ModulOperations) {
+                this.operations = new List<String>();
+                this.modulName = Modulname;
+                foreach (String moduloperation in ModulOperations) {
+                    operations.Add(moduloperation);
+                }
             }
         }   
         
@@ -47,6 +63,7 @@ namespace ConsoleApplication1
 
             public Int16 Group { get { return group; } set { group = value; } }
             public List<Int16> Elements { get { return elements; } set { elements = value; } }
+
             public GroupToDeleteInfo()
             {
                 elements = new List<Int16>();
@@ -55,7 +72,42 @@ namespace ConsoleApplication1
 
         public class Matrix:Dictionary<String,Dictionary<String,Boolean>>
         {
-            public Matrix():base() { }                
+            Dictionary<String,Int16> rowWeight;
+            Dictionary<String, Int16> columnWeight;
+
+            public Dictionary<String, Int16> RowWeight { get { return rowWeight; } set { rowWeight = value; } }
+            public Dictionary<String, Int16> ColumnWeight { get { return columnWeight; } set { columnWeight = value; } }
+
+            public void FindRowWeight() {
+                foreach(String Row in this.Keys) {
+                    rowWeight.Add(Row, 0);
+                    foreach(String Col in this[Row].Keys) {
+                        if(this[Row][Col] == true) {
+                            rowWeight[Row] += 1;
+                        }
+                    }
+                }
+            }
+            public void FindColWeight()
+            {
+                foreach(String Col in this.Keys)
+                {
+                    columnWeight.Add(Col, 0);
+                    foreach(String Row in this[Col].Keys)
+                    {
+                        if(this[Row][Col] == true)
+                        {
+                            columnWeight[Col] += 1;
+                        }
+                    }
+                }
+            }
+
+
+            public Matrix() : base() {
+                rowWeight = new Dictionary<string, short>();
+                columnWeight = new Dictionary<string, short>();
+            }                
         }
         
 }
